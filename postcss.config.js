3b5dad8154c4b7e8a1dbfa8e7733df6dfb87d565
Joshua <joshua.stuebner@bloxxter.com>
2022-08-04 17:24:43 +0200

    feat: small tweaks

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,11 @@ const postcssPresetEnv = require('postcss-preset-env');
 
 module.exports = {
   plugins: [
-    postcssPresetEnv({ stage: 1})
+    postcssPresetEnv({
+      stage: 1,
+      features: {
+        "focus-visible-pseudo-class": false
+      }
+    })
   ],
 }
